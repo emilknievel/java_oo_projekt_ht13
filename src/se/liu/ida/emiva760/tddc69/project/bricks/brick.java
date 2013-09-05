bd5a777eee44555554228e969b5327869eab2d6f
emilknievel <emil.vagstedt@gmail.com>
2013-09-05 14:18:59 +0200

    Initial commit. Before starting with any real coding, write a draft of the project description.

---
+++ src/se/liu/ida/emiva760/tddc69/project/bricks/brick.java
@@ -0,0 +1,11 @@
+package se.liu.ida.emiva760.tddc69.project.bricks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Emil
+ * Date: 2013-09-05
+ * Time: 14:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class brick {
+}
---
+++ src/se/liu/ida/emiva760/tddc69/project/gameboard/main.java
@@ -0,0 +1,11 @@
+package se.liu.ida.emiva760.tddc69.project.gameboard;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Emil
+ * Date: 2013-09-05
+ * Time: 14:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+}
