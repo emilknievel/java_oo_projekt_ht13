5d8dffda14af6d4390b980452c9f4fe2c52816de
emilknievel <emil.vagstedt@gmail.com>
2013-09-09 06:55:04 +0200

    The game objects should extend the Point2D.Double class.

--- src/se/liu/ida/emiva760/tddc69/project/bricks/brick.java
+++ src/se/liu/ida/emiva760/tddc69/project/bricks/brick.java
@@ -1,5 +1,7 @@
 package se.liu.ida.emiva760.tddc69.project.bricks;
 
+import java.awt.geom.Point2D;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Emil
@@ -7,5 +9,8 @@ package se.liu.ida.emiva760.tddc69.project.bricks;
  * Time: 14:00
  * To change this template use File | Settings | File Templates.
  */
-public class brick {
+public class brick extends Point2D.Double {
+    public brick(double x, double y) {
+        super(x, y);
+    }
 }
