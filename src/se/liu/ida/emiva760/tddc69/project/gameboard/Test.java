d31a6d2ba93e7e2c9357f7348d6fc1c42b995d0b
emilknievel <emil.vagstedt@gmail.com>
2013-09-11 06:35:44 +0200

    Test for window successful

--- src/se/liu/ida/emiva760/tddc69/project/gameboard/Test.java
+++ src/se/liu/ida/emiva760/tddc69/project/gameboard/Test.java
@@ -1,5 +1,7 @@
 package se.liu.ida.emiva760.tddc69.project.gameboard;
 
+import javax.swing.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Emil
@@ -7,5 +9,18 @@ package se.liu.ida.emiva760.tddc69.project.gameboard;
  * Time: 14:05
  * To change this template use File | Settings | File Templates.
  */
-public class Test {
+public class Test extends JFrame {
+    public Test()
+    {
+        setTitle("Breakout");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setIgnoreRepaint(true);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
 }
